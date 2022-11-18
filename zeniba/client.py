6e6154939f0ace61fdab2560cd671437b17ec66b
Pietro Jomini <pietro.jomini@protonmail.ch>
2022-11-18 11:47:48 +0100

    search params

--- zeniba/client.py
+++ zeniba/client.py
@@ -1,6 +1,8 @@
+from typing import List, Optional
 import requests
 
 import zeniba.config as config
+import zeniba.config.params as params
 
 
 class Client:
@@ -10,8 +12,43 @@ class Client:
         self.session.cookies["remix_userid"] = uid
         self.session.proxies = config.PROXIES
 
-    def search(self, query: str):
-        """Simple search"""
+    def search(
+        self,
+        query: str,
+        exact: bool = False,
+        yearFrom: Optional[int] = None,
+        yearTo: Optional[int] = None,
+        languages: List[str] = [],
+        extensions: List[str] = [],
+        order: str = "popuar",
+    ):
+        """
+        Simple search
+        """
 
-        res = self.session.get(f"{config.URL}/s/{query}")
+        # Params
+        # - e=1 [exact match] (+ "" to the query)
+        # - yearFrom={int}
+        # - yearTo={int}
+        # - languages[]={str}
+        # - extensions[]={str}
+        # - order={str}
+
+        # When not in allowed lists, they fallback to defaults or are ignored.
+
+        languages_extended = [
+            params.languages_s.get(l) if params.languages_s.get(l) is not None else l
+            for l in languages
+        ]
+
+        payload = {}
+        payload["e"] = "1" if exact else None
+        payload["yearFrom"] = yearFrom
+        payload["yearTo"] = yearTo
+        payload["languages[]"] = languages_extended
+        payload["extensions[]"] = extensions
+        payload["order"] = order
+
+        query = f'"{query}"' if exact else query
+        res = self.session.get(f"{config.URL}/s/{query}", params=payload)
         return res
